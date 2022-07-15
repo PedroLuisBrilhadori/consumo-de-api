bff43ce63c85fe4c5efb02b43f537366fd40818d
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-07-15 00:39:49 -0300

    refactor: cria função de setCard para mudar informações do card

--- src/cats.js
+++ src/cats.js
@@ -23,32 +23,41 @@ export async function getCat() {
  * @param element um elemento HTML (no caso do exemplo, uma div);
  */
 export async function setCat(imageElement) {
-  // elementos que serão usados para adicionar os dados no cat-card
-  const cardElement = document.getElementById("cat-card");
-  const cardUrlElement = document.getElementById("card-url");
-  const cardIdElement = document.getElementById("card-id");
   const style = "object-cover w-full max-h-80";
+
+  const image = {
+    element: imageElement,
+    src: '<img src="x" alt="x">',
+  };
 
   const response = await getCat();
 
   if (response.success) {
-    const data = response?.data;
-    let cat = `<img id="cat-photo" loading="lazy" src="${data.url}" class="${style}" alt="imagem do gatinho :3">`;
+    const data = response.data;
+    image.src = `<img id="cat-photo" loading="lazy" src="${data.url}" class="${style}" alt="imagem do gatinho :3">`;
 
-    imageElement.innerHTML = cat;
-    cardIdElement.innerText = data.id;
-    cardUrlElement.innerText = data.url;
-    cardElement.href = data.url;
-
+    setCard(data.id, data.url, image);
     return;
   }
 
   const httpCatUrl = `https://http.cat/${response?.status}`;
+  image.src = `<img id="cat-photo" loading="lazy" src="${httpCatUrl}" class="${style}" alt="imagem do gatinho :3">`;
 
-  let cat = `<img id="cat-photo" loading="lazy" src="${httpCatUrl}" class="${style}" alt="imagem do gatinho :3">`;
+  setCard("Nâo foi possível achar seu gatinho :(", httpCatUrl, image);
+}
 
-  imageElement.innerHTML = cat;
-  cardIdElement.innerText = "Não foi possivel achar seu gatinho :(";
-  cardUrlElement.innerText = httpCatUrl;
-  cardElement.href = httpCatUrl;
+/**
+ * Seta as informações do card
+ * @param id nome da foto
+ */
+function setCard(id, url, image) {
+  // elementos que serão usados para adicionar os dados no cat-card
+  const cardElement = document.getElementById("cat-card");
+  const cardUrlElement = document.getElementById("card-url");
+  const cardIdElement = document.getElementById("card-id");
+
+  image.element.innerHTML = image.src;
+  cardIdElement.innerText = id;
+  cardUrlElement.innerText = url;
+  cardElement.href = url;
 }
