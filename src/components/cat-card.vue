868ac20b5fb01c1b05e6d7baef57c1d286886f0f
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-07-15 02:17:43 -0300

    feat: fetch de imagens em vue

--- src/components/cat-card.vue
+++ src/components/cat-card.vue
@@ -1,4 +1,45 @@
-<script></script>
+<script>
+import { getCat } from "../cats";
+
+export default {
+  data() {
+    return {
+      cardState: {
+        id: "",
+        url: "",
+      },
+    };
+  },
+
+  methods: {
+    async setCard() {
+      const response = await getCat();
+
+      console.log(response);
+
+      if (!response.success) {
+        return;
+      }
+
+      const data = response.data;
+      this.setCardState(data.id, data.url);
+    },
+
+    setCardState(id, url) {
+      this.cardState.id = id;
+      this.cardState.url = url;
+    },
+
+    getCardState() {
+      return this.cardState;
+    },
+  },
+
+  mounted() {
+    this.setCard();
+  },
+};
+</script>
 
 <template>
   <a
@@ -7,12 +48,19 @@
     target="_blank"
     class="block overflow-hidden rounded-2xl m-6"
   >
-    <div id="card-photo"></div>
+    <img
+      :src="cardState.url"
+      loading="lazy"
+      alt="imagem do gatinho :3"
+      class="object-cover w-full max-h-80"
+    />
 
     <div class="p-4 bg-gray-900">
-      <h5 id="card-id" class="text-sm text-white"></h5>
+      <h5 class="text-sm text-white">{{ cardState.id }}</h5>
 
-      <p id="card-url" class="mt-1 text-xs text-gray-500"></p>
+      <p class="mt-1 text-xs text-gray-500">
+        {{ cardState.url }}
+      </p>
     </div>
   </a>
 </template>
