24879cbd67f8171ebdbb80f2277670babe825796
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-07-15 15:29:58 -0300

    feat: move a pasta assets para public

--- src/components/nav/nav.vue
+++ src/components/nav/nav.vue
@@ -10,7 +10,7 @@
     <div class="flex">
       <a href="https://rpjs.com.br" target="_blank">
         <img
-          src="../../assets/logo.svg"
+          src="assets/logo.svg"
           class="p-6 h-24 will-change-filter hover:drop-shadow-[0_0_2em_#f7df1eaa]"
           alt="RP.js logo"
         />
